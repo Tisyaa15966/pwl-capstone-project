6c8ff0115e1acfbf5798e71a3def2c4d2bf1ee1c
Tisyaa15966 <111202415966@mhs.dinus.ac.id>
2026-04-24 00:53:17 +0700

    Menyelesaikan Capstone 4

---
+++ belajar-ci5/app/Config/Routes.php
@@ -0,0 +1,15 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
+
+$routes->get('login', 'AuthController::login');// Menampilkan form
+$routes->post('login', 'AuthController::login');// Memproses data
+$routes->get('logout', 'AuthController::logout');// Keluar
+
+$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
